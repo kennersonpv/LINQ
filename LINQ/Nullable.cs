73ab2ad6d8b18d1d19a71beb8e676d9849c09508
kennersonpv <kennersondudu@hotmail.com>
2022-12-26 10:58:52 -0300

    first commit

---
+++ LINQ/Nullable.cs
@@ -0,0 +1,24 @@
+
+
+namespace System
+{
+    public class String
+    {
+        public void Shorten(int numberOfWords)
+        {
+
+
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class MessageArgs
+    {
+    }
+}
+
+
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace LINQ
+namespace Linq
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Func<int, int, int> add = (a, b) => a + b;
+            Console.WriteLine(add(2, 3));
         }
+
+        private static float CalculateDiscount(float price)
+        {
+            return 0;
+        }    
     }
 }
